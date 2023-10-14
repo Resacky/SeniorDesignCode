f11ae12f123136a72606b8ff26a9b474a907d356
Resacky <Ruiz-joseph@hotmail.com>
2023-10-14 13:56:36 -0400

    this is the prototype code for the raspberry pi

--- Script.py
+++ Script.py
@@ -1,10 +1,33 @@
 import asyncio
 import aioschedule as schedule
 import datetime
+import json
+import aiohttp
+
+async def fetch_data_from_api(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            return await resp.text()
 
 async def job():
-    print("Job running at", datetime.datetime.now())
+    try:
+        # This will be the API URL...
+        api_url = "YOUR_API_ENDPOINT_HERE"
+        response = await fetch_data_from_api(api_url)
+        json_data = json.loads(response)
+        
+        # For testing & debugging purposes...
+        # with open("./test_input/SignalK_testInput.json", "r") as file:
+            # json_data = json.load(file)
+        
+        # Extract boat's speed
+        speed_path = json_data["vessels"]["urn:mrn:signalk:uuid:521ae5ac-f557-4c7f-a225-e0e3de666ecb"]["navigation"]["speedThroughWater"]["value"]
+        speed_path_timestamp = json_data["vessels"]["urn:mrn:signalk:uuid:521ae5ac-f557-4c7f-a225-e0e3de666ecb"]["navigation"]["speedThroughWater"]["timestamp"]
 
+        print(f"Boat's speed: {speed_path} at {speed_path_timestamp}")
+    except Exception as e:
+        print(f"Error reading the file or extracting speed: {e}")
+ 
 async def main():
     schedule.every(10).seconds.do(job)  # Schedule job every 10 seconds
 
